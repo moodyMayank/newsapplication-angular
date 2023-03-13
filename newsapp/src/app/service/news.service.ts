69217e88664f815afd1b88eb50be50531948eb06
Mayank <mayankbudhiraja88@gmail.com>
2023-03-13 12:12:14 +0530

    Made all the data local

--- newsapp/src/app/service/news.service.ts
+++ newsapp/src/app/service/news.service.ts
@@ -26,12 +26,41 @@ export class NewsService {
 
     return this.http.get('https://api.npoint.io/91443291a9b4915c6175');
   }
+
+  // Commented the API , because it its cors is blocked and it only works on localhost
+
   getArticlesByid(source: string) {
-    return this.http.get(
-      'https://newsapi.org/v2/top-headlines?sources=' +
-        source +
-        '&apiKey=' +
-        this.api_key
-    );
+    // return this.http.get(
+    //   'https://newsapi.org/v2/top-headlines?sources=' +
+    //     source +
+    //     '&apiKey=' +
+    //     this.api_key
+    // );
+
+    let apiCall: string = '';
+    switch (source) {
+      case 'abc-news':
+        apiCall = 'https://api.npoint.io/c30a68b1eb1706bdf7e4';
+        break;
+      case 'abc-news-au':
+        apiCall = 'https://api.npoint.io/df076e6ccc86eb49e11d';
+        break;
+      case 'al-jazeera-english':
+        apiCall = 'https://api.npoint.io/d3ec52ae91bf63d8ea7c';
+        break;
+      case 'bbc-news':
+        apiCall = 'https://api.npoint.io/f20767c6c66e1dae4ee8';
+        break;
+      case 'bbc-sport':
+        apiCall = 'https://api.npoint.io/df9af10a95c33abfb28d';
+        break;
+      case 'bleacher-report':
+        apiCall = 'https://api.npoint.io/49abdd0dda651e889f57';
+        break;
+      default:
+        apiCall = 'https://api.npoint.io/91443291a9b4915c6175';
+    }
+
+    return this.http.get(apiCall);
   }
 }
