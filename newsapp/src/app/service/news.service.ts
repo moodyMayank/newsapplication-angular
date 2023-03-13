893b9df8813830a2b1d695b646884bb6c17fbf89
Mayank <mayankbudhiraja88@gmail.com>
2023-03-13 11:10:45 +0530

    Making apis local

--- newsapp/src/app/service/news.service.ts
+++ newsapp/src/app/service/news.service.ts
@@ -5,20 +5,26 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class NewsService {
+  // This API KEY can only be used in localHost because API provider only allow it to work on localhost
   api_key = 'eb3e45d4f56440c68ef3a48bb909b28a';
 
   constructor(private http: HttpClient) {}
+  // To use actual API , run the project in localhost and comment other returns
+
   initSources() {
-    return this.http.get(
-      'https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key
-    );
+    // return this.http.get(
+    //   'https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key
+    // );
+    return this.http.get('https://api.npoint.io/822adb5bcf997e7be6f4');
   }
 
   initArticles() {
-    return this.http.get(
-      'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' +
-        this.api_key
-    );
+    // return this.http.get(
+    //   'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' +
+    //     this.api_key
+    // );
+
+    return this.http.get('https://api.npoint.io/91443291a9b4915c6175');
   }
   getArticlesByid(source: string) {
     return this.http.get(
